912a7a3ce2e565a4bc9fa59bedb880c314ccb92d
Jhone-cmd <jhone.1941@gmail.com>
2026-08-17 14:06:28 -0300

    initial server configuration

--- src/main/java/com/jhonecmd/service_main/ServiceMainApplication.java
+++ src/main/java/com/jhonecmd/service_main/ServiceMainApplication.java
@@ -2,8 +2,12 @@ package com.jhonecmd.service_main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
 public class ServiceMainApplication {
 
 	public static void main(String[] args) {
